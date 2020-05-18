0fb00058bbf8c4650411f324d9bc1ba0643116fd
felixhalim <felix.halim@outlook.com>
2020-05-18 23:07:14 +0800

    Updates

---
+++ precache-manifest.ea4d2efa5541e93bb74cdee876438e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650ba3949a1617778a6849bb9022faa8",
+    "url": "/nusmods-doppelganger/index.html"
+  },
+  {
+    "revision": "c6ae1347181c41599009",
+    "url": "/nusmods-doppelganger/static/css/main.11e526f6.chunk.css"
+  },
+  {
+    "revision": "c734f09b5e0126617cb3",
+    "url": "/nusmods-doppelganger/static/js/2.e26011c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nusmods-doppelganger/static/js/2.e26011c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ae1347181c41599009",
+    "url": "/nusmods-doppelganger/static/js/main.b01f92e9.chunk.js"
+  },
+  {
+    "revision": "e74df5ad4abecd734298",
+    "url": "/nusmods-doppelganger/static/js/runtime-main.24315925.js"
+  }
+]);
